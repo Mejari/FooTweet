0e5b346f85914c46946883dcefa59ef77765afe5
Mejari <mejari@gmail.com>
2013-08-21 09:57:46 -0700

    Update testData.js

--- testData.js
+++ testData.js
@@ -2,10 +2,10 @@ var db = GLOBAL.db;
 
 var insertData = function() {
     db.TwitterAuthentication.create({
-        consumerKey: 'Dqvu8LooAAtoepRAdFlunQ',
-        consumerSecret: 'tGhE0N48ZKYIAKmAozUAEHG90kSZJpXPeNOP7QiIc',
-        accessTokenKey: '1654673785-Orbsozk8UBMR8drI76C2rKbGjZO6LMKfGc9TEiH',
-        accessTokenKeySecret: 'zZEr91AcaBK3rldO7pBBq5RNMN9ZJqCJhzjclgiPKqg'
+        consumerKey: '77777777777',
+        consumerSecret: '77777777',
+        accessTokenKey: '7-777777777777',
+        accessTokenKeySecret: '77777777777'
     }).success(function(ta){
         db.Account.create({
             name: 'LactosTolerance',
